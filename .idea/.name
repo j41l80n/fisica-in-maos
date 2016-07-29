6a47471be90c10bee9ec199ca4f726bb7c3631fe
jailson santos <jailson_donald_cobain@hotmail.com>
2016-07-29 14:49:23 -0300

    add HashApp

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+Física in Mãos
---
+++ app/src/main/java/raoni/ifrn/mestrado/fisicainmaos/helpers/HashApp.java
@@ -0,0 +1,47 @@
+package raoni.ifrn.mestrado.fisicainmaos.helpers;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Jailson on 29/07/2016.
+ */
+
+public class HashApp extends Application
+{
+    public void onCreate()
+    {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey()
+    {
+        try
+        {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "raoni.ifrn.mestrado.fisicainmaos.helpers", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures)
+            {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("printHashKey: ", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            //
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            //
+        }
+    }
+}
