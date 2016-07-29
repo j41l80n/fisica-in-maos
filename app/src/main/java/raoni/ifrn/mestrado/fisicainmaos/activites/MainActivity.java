be101d0b04df1b628bc05def06ae910da5579f23
jailson santos <jailson_donald_cobain@hotmail.com>
2016-07-29 14:38:04 -0300

    This is where it all begins...

---
+++ app/src/main/java/raoni/ifrn/mestrado/fisicainmaos/activites/MainActivity.java
@@ -0,0 +1,15 @@
+package raoni.ifrn.mestrado.fisicainmaos.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import raoni.ifrn.mestrado.fisicainmaos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
